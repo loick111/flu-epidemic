be7f26cd39445df2b41afb29bd1836c2236e7233
Loïck MAHIEUX <loick111@gmail.com>
2015-11-30 11:05:23 +0100

    init

---
+++ src/main/java/fr/loick/App.java
@@ -0,0 +1,12 @@
+package fr.loick;
+
+/**
+ * Flu Epidemic App
+ * @author Loïck MAHIEUX
+ * @date 30/11/2015
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Flu Epidemic!" );
+    }
+}
